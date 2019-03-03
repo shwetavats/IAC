4e1b75d01b750ef0a876f886e1727824803c8625
EC2 Default User <ec2-user@ip-172-31-7-191.ap-southeast-1.compute.internal>
2019-03-03 20:31:56 +0000

    adding ec2 module

--- terraform/beta/beta.tf
+++ terraform/beta/beta.tf
@@ -10,3 +10,13 @@ module IAC_VPC {
     IACpubcidr2 = "${var.envIACpubcidr2}"
     IACprivcidr1="${var.envIACprivcidr1}"
 }
+module IAC_EC2 { 
+    source = "../modules/IAC_EC2"
+    modami="${var.envmodami}"
+    modinstancetype="${var.envmodinstancetype}"
+    modkeypair = "${var.envmodkeypair}"
+    modinstancepubsubnet="${var.envmodinstancepubsubnet}"
+    modenvtype = "${var.envmodenvtype}"
+    modvpc_id = "${var.envmodvpc_id}"
+}
+
